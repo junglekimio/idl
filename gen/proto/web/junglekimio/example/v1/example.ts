f1afd1c91c8f6a457313b18f769c84d0d7478f5a
Jeonggeun Kim <snorlax.jungle@gmail.com>
2022-09-23 21:22:47 +0900

    Junglekim/feat/change pkg (#4)
    * Update proto
    
    * Clean generated files
    
    * Rename workspace
    
    * change: java_outer_classname
    
    * Update buf.lock

---
+++ gen/proto/web/junglekimio/example/v1/example.ts
@@ -0,0 +1,17 @@
+/* eslint-disable */
+
+export const protobufPackage = "junglekimio.example.v1";
+
+export interface HealthCheckRequest {
+}
+
+export interface HealthCheckResponse {
+}
+
+export interface HelloRequest {
+  name: string;
+}
+
+export interface HelloResponse {
+  name: string;
+}
